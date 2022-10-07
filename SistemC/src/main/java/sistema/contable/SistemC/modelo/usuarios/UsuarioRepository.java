28b7044a73aafcd9e721c289c866111d09581184
Juan Carlos <jcs1011@gmail.com>
2022-10-07 17:58:43 -0500

    se crea clases usuariorepository

---
+++ SistemC/src/main/java/sistema/contable/SistemC/modelo/usuarios/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package sistema.contable.SistemC.modelo.usuarios;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuarios, Long>
+{
+    
+}
